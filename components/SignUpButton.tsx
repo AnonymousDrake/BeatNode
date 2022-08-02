331fcdf2019c51b086a7809b97320efb0a81fe2b
Diviyansh Modi <71758538+modidiviyansh@users.noreply.github.com>
2022-08-02 21:50:15 +0530

    Updated Buttons

--- components/SignUpButton.tsx
+++ components/SignUpButton.tsx
@@ -1,10 +1,9 @@
 const SignUpButton = ({ label = 'SignIn' }) => {
   return (
     <div className="bg-white padding: 5px">
-      <br></br>
       <button
         type="button"
-        className="inline-block border-2 border-rose-500 px-6 py-2.5 bg-white-600 text-wine font-poppins font-semibold text-xs leading-tight rounded-lg shadow-md "
+        className="inline-block px-6 py-2.5 bg-white-600 text-wine font-poppins font-semibold text-xs leading-tight rounded-lg shadow-md "
       >
         {label}
       </button>
